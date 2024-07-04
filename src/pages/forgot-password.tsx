7d2f3a610ca404729770b439644c0e82c4555935
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-07-04 23:39:05 +0200

    added img on forgot-password page

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Layout from "@/components/Layout";
 
 const ForgotPasswordSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -47,46 +48,50 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold mb-4">Forgot your password?</h2>
-        <p className="mb-4">
-          Do not worry, it happens. Let us know the email address you signed up
-          with and we will send you an email with instructions.
-        </p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            {...register("email")}
-            className={`mt-1 block w-full px-3 py-2 border ${
-              errors.email ? "border-red-500" : "border-gray-300"
-            } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-          />
-          {errors.email && (
-            <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
-          )}
-          <button
-            type="submit"
-            className="mt-4 w-full py-2 px-4 bg-green-600 hover:bg-green-800 text-white font-bold rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-          >
-            Send Reset Link
-          </button>
-        </form>
-        {message && <p className="mt-4 text-sm text-gray-600">{message}</p>}
-        <Link href="/login">
-          <p className="mt-4 text-sm text-blue-500 hover:underline">
-            ← Back to Login
+    <Layout imgUrl="/background-images/login-page-background.jpg">
+      <div className="flex flex-col items-center justify-center min-h-screen ">
+        <div className="w-full max-w-md p-8 bg-opacity-80 shadow-md rounded-lg bg-gray-100 backdrop-blur">
+          <h2 className="text-2xl font-bold mb-4">Forgot your password?</h2>
+          <p className="mb-4">
+            Do not worry, it happens. Let us know the email address you signed
+            up with and we will send you an email with instructions.
           </p>
-        </Link>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...register("email")}
+              className={`mt-1 block w-full px-3 py-2 border ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.email.message}
+              </p>
+            )}
+            <button
+              type="submit"
+              className="mt-4 w-full py-2 px-4 bg-green-600 hover:bg-green-800 text-white font-bold rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+            >
+              Send Reset Link
+            </button>
+          </form>
+          {message && <p className="mt-4 text-sm text-gray-600">{message}</p>}
+          <Link href="/login">
+            <p className="mt-4 text-sm text-blue-500 hover:underline">
+              ← Back to Login
+            </p>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
