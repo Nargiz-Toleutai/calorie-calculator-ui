3dec4bdde1199d72c586c53a5c3b9e6b7a5084e3
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-07-10 13:29:07 +0200

    fixed products css

--- src/components/Product/ProductItem.tsx
+++ src/components/Product/ProductItem.tsx
@@ -28,7 +28,11 @@ const ProductItem = ({
   return (
     <Link href={`/edit-product/${id}`} className="justify-end">
       <div className="border rounded-lg p-4 shadow-lg flex flex-col items-center bg-white bg-opacity-80 backdrop-blur font-medium transform transition-transform duration-300 hover:scale-105">
-        <img src={image} alt={name} className="w-32 h-32 object-cover mb-4" />
+        <img
+          src={image}
+          alt={name}
+          className="w-32 h-32 object-cover mb-4 rounded-lg"
+        />
         <h2 className="text-lg font-bold mb-2">{name}</h2>
         <p className="text-green-500 font-bold">
           {protein} {unit} Protein
